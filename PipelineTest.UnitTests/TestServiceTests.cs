0c3acd05010ba0874fd7ae36f02a241675dcd48b
Lucas.Christinson <Lucas.Christinson@nordsonasymtek.com>
2018-11-14 12:28:08 -0800

    Simple test project

---
+++ PipelineTest.UnitTests/TestServiceTests.cs
@@ -0,0 +1,28 @@
+﻿using Xunit;
+
+namespace PipelineTest.UnitTests
+{
+    public class TestServiceTests
+    {
+        [Fact]
+        public void StartServiceTest()
+        {
+            var sut = new TestService();
+            Assert.False(sut.IsRunning);
+
+            sut.Start();
+            Assert.True(sut.IsRunning);
+        }
+
+        [Fact]
+        public void StopServiceTest()
+        {
+            var sut = new TestService();
+            sut.Start();
+            Assert.True(sut.IsRunning);
+
+            sut.Stop();
+            Assert.False(sut.IsRunning);
+        }
+    }
+}
---
+++ PipelineTest/TestService.cs
@@ -0,0 +1,17 @@
+﻿namespace PipelineTest
+{
+    public class TestService
+    {
+        public bool IsRunning { get; private set; }
+
+        public void Start()
+        {
+            IsRunning = true;
+        }
+
+        public void Stop()
+        {
+            IsRunning = false;
+        }
+    }
+}
